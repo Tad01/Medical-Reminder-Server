52bf281664043d166a89854748751e4ac066164c
Tad <tandatit19@gmail.com>
2019-06-12 19:51:29 +0700

    pdate...

--- src/config/db.js
+++ src/config/db.js
@@ -10,7 +10,7 @@ const common = {
   dbPostgresPort: 5432,
   dbPostgresSSLPort: 5572,
   dbPostgresDialect: 'postgres',
-  dbPostgresURI: 'postgres://uedoajrdoowuzt:b0392e59e8e25102b9fdc25d94ced9e438bf2a140a61797e8d3b80b03e2fae05@ec2-23-21-129-125.compute-1.amazonaws.com:5432/dbusihubg613d4',
+  dbPostgresURI: 'postgres://dqvbrxrokancky:f98c260c9c57539aae4fc6013ec2eaf3a4afdbaeb730b24940e2ddd928291593@ec2-50-19-114-27.compute-1.amazonaws.com:5432/dfr8v2qroholub',
 
   dbPostgresOptions: {
     define: {
