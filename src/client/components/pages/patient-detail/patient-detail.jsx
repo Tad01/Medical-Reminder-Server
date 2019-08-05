83bd62cde1a0b60d34757df8999cf0cc9c42da84
Tad <tandatit19@gmail.com>
2019-08-05 09:00:18 +0700

    update

--- src/client/components/pages/patient-detail/patient-detail.jsx
+++ src/client/components/pages/patient-detail/patient-detail.jsx
@@ -115,23 +115,8 @@ export default class extends Component {
                   </div>
                 )) : null}
               </div>
-              {/* <nav aria-label="Page navigation example ">
-                <ul className="pagination justify-content-center ">
-                  <li className="page-item disabled page">
-                    <a className="page-link" href="/#" tabIndex="-1">Previous</a>
-                  </li>
-                  <li className="page-item"><a className="page-link" href="/#">1</a></li>
-                  <li className="page-item"><a className="page-link" href="/#">2</a></li>
-                  <li className="page-item"><a className="page-link" href="/#">3</a></li>
-                  <li className="page-item">
-                    <a className="page-link" href="/#">Next</a>
-                  </li>
-                </ul>
-              </nav> */}
             </div>
           </div>
-
-
         </div>
       </React.Fragment>
     );
